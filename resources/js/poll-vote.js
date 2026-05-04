0d16078aae5793b62e4f06f75cb780f554b82e78
DYLANeay <dylan.eray6@gmail.com>
2026-05-04 15:45:05 +0200

    create poll-vote.js, to pass the data to the app

---
+++ resources/js/poll-vote.js
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import AppPollVote from "./AppPollVote.vue";
+
+const el = document.getElementById("app");
+const props = JSON.parse(el.dataset.props);
+
+createApp(AppPollVote, props).mount(el);
