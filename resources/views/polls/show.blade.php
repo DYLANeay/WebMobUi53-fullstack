d41a2d5408edc14187b2a231daa7550b96286f89
DYLANeay <dylan.eray6@gmail.com>
2026-05-04 15:43:43 +0200

    add view to show the poll vote and inject js (that injects vue)

---
+++ resources/views/polls/show.blade.php
@@ -0,0 +1,11 @@
+<x-vue-app-layout>
+    <x-slot:scripts>
+        @vite(['resources/js/poll-vote.js'])
+    </x-slot>
+
+    <x-slot:title>
+        {{ $poll->question }}
+    </x-slot>
+
+    <div id="app" data-props="{{ json_encode(['poll' => $poll]) }}"></div>
+</x-vue-app-layout>
