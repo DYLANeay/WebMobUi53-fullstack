10d659675af934a689593299e5271f9edece01f0
DYLANeay <dylan.eray6@gmail.com>
2026-05-05 18:35:56 +0200

    add error fallback

--- resources/js/poll-vote.js
+++ resources/js/poll-vote.js
@@ -3,6 +3,6 @@ import { createApp } from "vue";
 import AppPollVote from "./AppPollVote.vue";
 
 const el = document.getElementById("app");
-const props = JSON.parse(el.dataset.props);
+const props = JSON.parse(el.dataset.props ?? "{}");
 
 createApp(AppPollVote, props).mount(el);
